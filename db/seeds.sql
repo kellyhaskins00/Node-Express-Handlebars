7946bff26fb4d753f221a86a3e1283e0b7a65cdc
Kelly Haskins <kellyhaskins00@gmail.com>
2019-01-26 14:10:06 -0800

    finished burger app

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', true);
+
+
+
